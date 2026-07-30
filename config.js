6a75a16d2e6fc3a10eba1033423eb2d650510645
pollyferreira2003-glitch <pollyferreira2003@gmail.com>
2026-07-30 18:52:55 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyKheC340QYCZvEn-rEDoF--C-KDMhTxdPZfvY8_NJ_oWsKgg-U58W7DN-rprRrqVvgZA/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwYoUPb6bzvUw1yiSAqI1vzGuI68VN6srO8dKT6XYK7YjuZRMtPaEz0uV_mRehXbHpSig/exec"
 };
